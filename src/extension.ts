b3fbcc47058fba59e5929432288864c50090d776
Mees Frensel <meesfrensel@gmail.com>
2018-04-28 12:21:37 +0200

    Add simple (partially example) language server

---
+++ src/extension.ts
@@ -0,0 +1,46 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) DigitalBrainstem
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import * as path from 'path';
+
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('out', 'src', 'server.js'));
+
+	// The debug options for the server
+	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	}
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for ejs files
+		documentSelector: [{ language: 'ejs', scheme: 'file' }],
+		synchronize: {
+			// Synchronize the setting section 'ejs-support' to the server
+			configurationSection: 'ejs-support',
+			// Notify the server about file changes to '.clientrc' files contain in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	}
+
+	// Create the language client and start the client.
+	let disposable = new LanguageClient('ejs-support-client', 'EJS language support extension',
+		serverOptions, clientOptions).start();
+
+	// Push the disposable to the context's subscriptions so that the 
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(disposable);
+}
